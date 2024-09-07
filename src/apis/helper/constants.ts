2db3a44c7f5d5e5c8c9c1a07c2a3fe8070ef4104
Mohsin Attar <mohsinattar07@gmail.com>
2024-09-07 22:55:00 +0530

    deployment fix 5

--- src/apis/helper/constants.ts
+++ src/apis/helper/constants.ts
@@ -1,3 +1,3 @@
 export const baseURL = process.env.NODE_ENV === "development"
     ? "http://localhost:3000/api" // development api
-    : "http://localhost:3000/api" // production api
+    : "https://rs-motors-git-main-mohasins-projects.vercel.app/api" // production api
