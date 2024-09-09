f6cb6e071b11fa2a2b7681a0cf001601c1854d4c
Mohsin Attar <mohsinattar07@gmail.com>
2024-09-09 14:59:15 +0530

    PROD FIX

--- src/utils/vehicleServices.ts
+++ src/utils/vehicleServices.ts
@@ -1,21 +1,25 @@
 import { jsonConfig } from "@/apis/common.service"
 import { baseURL } from "@/apis/helper/constants"
+import dbConnect from "@/app/lib/db"
 import { newStock, Vehicle } from "@/types"
 import axios from "axios"
 
 const url = `${baseURL}/vehicles`
 
 const getAll = async (): Promise<Vehicle[]> => {
+    await dbConnect()
     const response = await axios.get(`${url}/`, jsonConfig)
     return response.data
 }
 
 const getVehicleDetail = async (id: string): Promise<Vehicle> => {
+    await dbConnect()
     const response = await axios.get(`${url}/${id}`)
     return response.data
 }
 
 const getNewStock = async (): Promise<newStock[]> => {
+    await dbConnect()
     const response = await axios.get(`${url}/getNewStock`, jsonConfig)
     return response.data
 }
