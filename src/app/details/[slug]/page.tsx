dca2ac0b5e931a0e85073ecb90720252031c9e2a
Mohsin Attar <mohsinattar07@gmail.com>
2024-09-09 14:05:16 +0530

    dashboard api fix

--- src/app/details/[slug]/page.tsx
+++ src/app/details/[slug]/page.tsx
@@ -1,24 +1,28 @@
-import { vehicleServices } from "@/apis/vehicle.service";
+import { jsonConfig } from "@/apis/common.service";
+import { baseURL } from "@/apis/helper/constants";
 import { Details } from "@/components";
 import { Vehicle } from "@/types";
+import axios from "axios";
 import React from "react";
 
 export const revalidate = 60;
 export const dynamicParams = true;
 
-const fetchVehicleDetails = async (id: string) => {
-  return await vehicleServices.getVehicleDetail(id);
+const url = `${baseURL}/vehicles`;
+
+const fetchVehicleDetails = async (id: string): Promise<Vehicle> => {
+  const response = await axios.get(`${url}/${id}`, jsonConfig);
+  return response.data;
 };
 
 export async function generateStaticParams() {
-  let data = await vehicleServices.getAll();
-  return data?.map((obj) => ({
-    id: obj._id,
-  }));
+  let { data }: { data: Vehicle[] } = await axios.get(`${url}/`, jsonConfig);
+  return data.map((obj) => {
+    id: obj._id;
+  });
 }
 
 // { params }: { params: { slug: string } }
-
 
 const VehicleDetail = async ({ params }: { params: { slug: string } }) => {
   let data = await fetchVehicleDetails(params?.slug);
